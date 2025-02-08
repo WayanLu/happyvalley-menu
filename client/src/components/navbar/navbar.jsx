a66c9831ad374a5c35fb1a2f154f6bea10e5f236
WayanLu <ryliu3@gmail.com>
2025-02-07 19:08:06 -0800

    renamed file ext to jsx

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -5,22 +5,21 @@ function CreateNavbar() {
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
         {/* Navbar Brand */}
-        <Navbar.Brand href="/home">My Site</Navbar.Brand>
+        <Navbar.Brand href="/home">
+          Happy Valley Seafood Restaurant
+        </Navbar.Brand>
 
         {/* Navbar Toggle for mobile */}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           {/* Left-aligned nav items */}
           <Nav>
-            <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/projects">Projects</Nav.Link>
+            <Nav.Link href="/home">Menu</Nav.Link>
           </Nav>
 
           {/* Right-aligned nav items */}
           <Nav className="ms-auto">
-            <Nav.Link href="/profile">Profile</Nav.Link>
-            <Nav.Link href="/logout">Logout</Nav.Link>
+            <Nav.Link href="/logout">Admin</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
