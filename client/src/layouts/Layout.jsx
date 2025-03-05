a5fd180a189d710d166aac58d17f479ee90e6bc8
WayanLu <ryliu3@gmail.com>
2025-03-04 22:23:06 -0800

    added admin page to layouts

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import CreateNavbar from "../components/navbar/navbar";
 import CreateMenu from "../pages/Menu";
-//import Admin from "../pages/Admin";
+import CreateAdmin from "../pages/Admin";
 
 function Layout() {
   return (
@@ -12,7 +12,7 @@ function Layout() {
       <Container className="mt-3">
         <Routes>
           <Route path="/" element={<CreateMenu />} />
-          <Route path="/admin" element={<h1>test</h1>} />
+          <Route path="/admin" element={<CreateAdmin />} />
         </Routes>
       </Container>
     </div>
