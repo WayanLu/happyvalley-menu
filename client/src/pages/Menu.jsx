2e1d13dac37cef4512a0d7f931b4c31bbc4c603c
WayanLu <ryliu3@gmail.com>
2025-03-13 15:32:47 -0700

    changed layout of menu items

--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -1,5 +1,8 @@
 import React, { useMemo } from "react";
 import { Tab, Tabs } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import MenuList from "../features/menu/MenuList";
 import menuData from "../data.json";
 
@@ -17,22 +20,75 @@ function CreateMenu() {
   }, []); */
 
   return (
-    <Tabs
-      defaultActiveKey="Appetizers"
-      id="fill-tab-example"
-      className="mb-3"
-      fill
-    >
-      {/* Iterate through categories */}
-      {Object.entries(menuData.menu).map(([category, items]) => (
-        <Tab key={category} eventKey={category} title={category}>
-          <div className="items-list">
-            <MenuList categoryList={items} />
-          </div>
-        </Tab>
-      ))}
-    </Tabs>
+    <Container>
+      <Row>
+        {/* Categories*/}
+        <Col xs={4} md={3} lg={2}>
+          {Object.entries(menuData.menu).map(([category, items]) => (
+            <Row key={category}>{category}</Row>
+          ))}
+        </Col>
+        {/* Menu Items*/}
+        <Col xs={8} md={9} lg={10}>
+          {Object.entries(menuData.menu).map(([category, items]) => (
+            <div key={category} className="{category}">
+              <h1>{category}</h1>
+              <div className={`${category}-items`}>
+                {items.map((item) => (
+                  <Row key={item.id}>{item.name}</Row>
+                ))}
+              </div>
+            </div>
+          ))}
+        </Col>
+      </Row>
+    </Container>
   );
+}
+{
+  /* <Tabs */
+}
+{
+  /*   defaultActiveKey="Appetizers" */
+}
+{
+  /*   id="fill-tab-example" */
+}
+{
+  /*   className="mb-3" */
+}
+{
+  /*   fill */
+}
+{
+  /* > */
+}
+{
+  /*   {/* Iterate through categories */
+}
+{
+  /*   {Object.entries(menuData.menu).map(([category, items]) => ( */
+}
+{
+  /*     <Tab key={category} eventKey={category} title={category}> */
+}
+{
+  /*       <div className="items-list"> */
+}
+{
+  /*         <MenuList categoryList={items} /> */
+}
+{
+  /*       </div> */
+}
+{
+  /*     </Tab> */
+}
+{
+  /*   ))} */
+}
+{
+  /* </Tabs> */
 }
 
 export default CreateMenu;
